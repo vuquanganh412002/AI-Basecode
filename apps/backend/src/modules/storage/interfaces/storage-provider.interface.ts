6406203de5e88039d230a3f6a1e24d6469269bd7
quanh <vuquanganh412002@gmail.com>
2026-05-21 21:55:11 +0700

    clean source

--- apps/backend/src/modules/storage/interfaces/storage-provider.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface StorageProvider {
-  upload(key: string, body: Buffer, contentType: string): Promise<string>;
-  download(key: string): Promise<Buffer>;
-  getSignedUrl(key: string, expiresIn?: number): Promise<string>;
-  delete(key: string): Promise<void>;
-}
